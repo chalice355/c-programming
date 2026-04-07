f1869a2a8e458372655f21489e5305ad96daf35f
chalice355 <galaxy355@naver.com>
2026-04-07 19:49:50 +0900

    Update 4-2-5.cpp

--- chap 04/4-2/4-2-5.cpp
+++ chap 04/4-2/4-2-5.cpp
@@ -2,27 +2,29 @@
 using namespace std;
 
 int main() {
-	cout << "저장할 문자열의 크기를 입력하세요:";
+	cout << "저장할 문자열의 크기를 입력하세요: ";
 	int n;
 	cin >> n;
+	cin.ignore();
 	if (n <= 0) return -1;
-	char* p = new char[n];
+	char* p = new char[n + 1];
 	if (!p) {
 		cout << "메모리를 할당할 수 없습니다.";
 		return -1;
 	}
-	cout << "문자열을 입력 하시오." << endl;
-	cin >> p;
+
+	cout << "문자열을 입력 하시오: " << endl;
+	cin.getline(p, n + 1, '\n');
 	cout << "입력한 문자열은 ";
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; p[i] != '\0'; i++)
 		cout << p[i];
-	}
 	cout << "입니다." << endl;
 	delete[] p;
 }
 
 /*
-저장할 문자열의 크기를 입력하세요: 11<엔터>
-문자열을 입력하시오: I am a boy.<엔터>
+저장할 문자열의 크기를 입력하세요: 11
+문자열을 입력 하시오:
+I am a boy.
 입력한 문자열은 I am a boy.입니다.
 */
