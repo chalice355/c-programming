912e62c8bb36f5f8fafec194398c44d86624f6d9
chalice355 <galaxy355@naver.com>
2026-05-18 10:35:18 +0900

    Add files via upload

---
+++ chap 06/6-1/6-1-3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Rectangle {
+	int width, height;
+public:
+	Rectangle(int w = 1, int h = 1) : width(w), height(h) {}
+	void show() {
+		cout << "사각형 폭은 " << width << "높이는 " << height << endl;
+	}
+};
+
+int main() {
+	Rectangle rect0;
+	rect0.show();
+	Rectangle rect1(10);
+	rect1.show();
+	Rectangle rect2(10, 20);
+	rect2.show();
+	return 0;
+}
+
+/*
+사각형 폭은 1 높이는 1
+사각형 폭은 10 높이는 1
+사각형 폭은 10 높이는 20
+*/
