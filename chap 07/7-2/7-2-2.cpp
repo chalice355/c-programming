0341bc647edad225681e6a72fdaa2f61a1cb2e67
chalice355 <galaxy355@naver.com>
2026-06-02 21:45:47 +0900

    Update 7-2-2.cpp

--- chap 07/7-2/7-2-2.cpp
+++ chap 07/7-2/7-2-2.cpp
@@ -12,7 +12,7 @@ public:
 };
 
 void Complex::show() {
-	if (img < 0)	cout << real << "-" << img << "j" << endl;
+	if (img < 0)	cout << real << img << "j" << endl;
 	else if (img >= 0)	cout << real << "+" << img << "j" << endl;
 }
 
